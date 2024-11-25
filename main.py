64cccdcccf7d0606dcd0f61b0e1f29bdd0582dc8
Kate B. <katerinabatunina@gmail.com>
2024-11-25 18:21:43 +0100

    working bot: 3 commands

--- main.py
+++ main.py
@@ -1,14 +1,55 @@
+import asyncio
+import datetime
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
-# создать дикт со значениями тестовьіми
-with open('nov.csv') as schedule:
-    content = schedule.readlines()
-    # print(type(content))
-    print(content[0])
+from settings import TOKEN
 
-# def today_shift():
-#     date = (input('write today is date:'))
-#     if date in content:
-#         print('true')
-#
-#
-# print(today_shift())
+dp = Dispatcher()
+
+
+class Schedule:
+    schedule_list = []
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.reply("Hey, welcome!")
+
+
+@dp.message()
+async def upload_chesdule(message: Message):
+    match message.text:
+        case "/today":
+            try:
+                await message.answer(s.schedule_list[datetime.datetime.now().day - 1])
+            except:
+                await message.answer("Update your schedule")
+        case "/tomorrow":
+            try:
+                await message.answer(s.schedule_list[datetime.datetime.now().day])
+            except:
+                await message.answer("Update your schedule")
+        case "/weekend":
+            today = datetime.datetime.now().day
+            rest_of_the_month = Schedule.schedule_list[today:]
+            for i, day in enumerate(rest_of_the_month):
+                if day == "L":
+                    await message.answer(f"Your weekend is in {i + 1} days")
+        case _:
+            schedule = message.text.split()
+            Schedule.schedule_list = schedule
+            await message.answer("Your schedule is list now!")
+
+
+async def main() -> None:
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    s = Schedule()
+    asyncio.run(main())
